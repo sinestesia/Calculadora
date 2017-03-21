5d2444b095d29cefb3aea393569ff641ebcc1f03
sinestesia <reyesdelsuenio@gmail.com>
2017-03-21 20:53:30 +0100

    Initial commit

---
+++ app/src/main/java/es/pamp/calculadora/MainActivity.java
@@ -0,0 +1,24 @@
+package es.pamp.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String valor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String op = savedInstanceState.getString("operacion");
+    }
+
+   /* @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(valor);
+        super.onSaveInstanceState(outState);
+
+    }*/
+}
